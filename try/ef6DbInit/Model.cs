0dd05f496def1b1e8f92aed5b26a42e5c0564d8b
Sergey Ermolenko <sergey.ermolenkoo@gmail.com>
2023-08-25 21:25:12 -0700

    try db initi

---
+++ try/ef6DbInit/Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Data.Entity;
+
+namespace ef6DbInit
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(string nameOrConnectionString) :base(nameOrConnectionString)
+        {
+
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+
+        [Required]
+        public virtual Blog Blog { get; set; }
+    }
+}
---
+++ try/ef6DbInit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ef6DbInit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = "Server=localhost,1433;Database=ef6GetStarted;Trusted_Connection=True;";
+
+            using (var db = new BloggingContext(connStr))
+            {
+                // Note: This sample requires the database to be created before running.
+
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post { Title = "Hello World", Content = "An EF6 App!" });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Blogs.Remove(blog);
+                db.SaveChanges();
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
